a89aa509bbb4d516b382a2941a44cd89a63cdfac
Tim Licata <tim@timothylicata.com>
2016-08-08 18:15:54 -0700

    change deprecated conn() to build_conn()
    warning: using conn/0 to build a connection is deprecated. Use build_conn/0 instead.
        test/controllers/page_controller_test.exs:5: Mighty.PageControllerTest."test GET /"/1
        (ex_unit) lib/ex_unit/runner.ex:296: ExUnit.Runner.exec_test/1
        (stdlib) timer.erl:166: :timer.tc/1
        (ex_unit) lib/ex_unit/runner.ex:246: anonymous fn/3 in ExUnit.Runner.spawn_test/3

--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -2,7 +2,7 @@ defmodule Mighty.PageControllerTest do
   use Mighty.ConnCase
 
   test "GET /" do
-    conn = get conn(), "/"
+    conn = get build_conn(), "/"
     assert html_response(conn, 200) =~ "Hello"
   end
 end
